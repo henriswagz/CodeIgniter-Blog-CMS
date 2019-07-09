bc611f45d199d2503ee9fd9f0363e2754b05d1a8
henriswagz <henrialabi@gmail.com>
2019-07-09 11:45:38 +0100

    Added the git deployment script

---
+++ application/views/templates/header.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>ciBlog</title>
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-8+rznmq/k0KZkJlZhnuPEVkbRD7tA0wcFEjY48dajGWn3Xc1MasJwS8/tJ7OEsKW" crossorigin="anonymous">
+
+
+		<!--<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">-->
+		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+		<script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="<?php echo base_url();?>">ciBlog</a>
+				</div>
+				<div id="navbar">
+					<ul class="nav navbar-nav">
+						<li><a href="<?php echo base_url();?>">Home</a></li>
+						<li><a href="<?php echo base_url();?>about">About</a></li>
+						<li><a href="<?php echo base_url();?>posts">Blog</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="<?php echo base_url();?>posts/create">Create Post</a></li>
+
+					</ul>
+				</div> 
+			</div>
+		</nav>
+
+		<div class="container">
+
+
